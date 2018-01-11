4137dfe8b6382da3a55de2cde7f0d94f7f9f856a
LexisR <lexisrodriguez22@gmail.com>
2018-01-11 18:08:11 -0300

    Agrgando seccion de login, Api de google

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,3 +90,61 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
 });
 
 }())
+
+var map;
+var infowindow;
+
+function initMap()
+{
+// Creamos un mapa con las coordenadas actuales
+ navigator.geolocation.getCurrentPosition(function(pos) {
+
+ lat = pos.coords.latitude;
+ lon = pos.coords.longitude;
+
+ var myLatlng = new google.maps.LatLng(lat, lon);
+
+ var mapOptions = {
+   center: myLatlng,
+   zoom: 16,
+ };
+
+ map = new google.maps.Map(document.getElementById("map"),  mapOptions); //este es el mapa.
+
+ // Creamos el infowindow que nos dara info del pin
+ infowindow = new google.maps.InfoWindow();
+
+ // Especificamos la localización, el radio y el tipo de lugares que queremos obtener
+ var request = {
+   location: myLatlng,
+   radius: 5000,
+   types: ['museum','park']
+ };
+
+ // Creamos el servicio PlaceService y enviamos la petición.
+ var service = new google.maps.places.PlacesService(map);
+
+ service.nearbySearch(request, function(results, status) {
+   if (status === google.maps.places.PlacesServiceStatus.OK) {
+     for (var i = 0; i < results.length; i++) {
+       crearMarcador(results[i]);
+     }
+   }
+ });
+});
+}
+
+function crearMarcador(place)
+{
+ // Creamos un marcador
+ var marker = new google.maps.Marker({
+   map: map,
+   position: place.geometry.location
+ });
+
+// Asignamos el evento click del marcador para que nos muestre la info
+ google.maps.event.addListener(marker, 'click', function() {
+   infowindow.setContent(place.name);
+   infowindow.open(map, this);
+ });
+ }
